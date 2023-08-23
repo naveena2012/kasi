90dfd36ec7a1fb7817141e64e32de464e1a88b0b
naveena2012 <nalininalene2012@gmail.com>
2023-08-23 10:57:07 +0530

    my first commit

---
+++ src/test/java/git/Test1.java
@@ -0,0 +1,14 @@
+package git;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void method1()
+	    {
+			System.out.println("hii all");
+            Reporter.log("hii, please make me soo");		
+	    }
+}
+		
